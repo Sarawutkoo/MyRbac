81ffa9e2806493cd553e29015fe11e2c5f4f9e53
sarawut <moskoo12355@gmail.com>
2017-08-05 15:31:21 +0700

    before Lunch

--- app/src/main/java/com/koo/sarawut/myrbac/MainActivity.java
+++ app/src/main/java/com/koo/sarawut/myrbac/MainActivity.java
@@ -3,11 +3,24 @@ package com.koo.sarawut.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.koo.sarawut.myrbac.frigment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContrainer, mainFragment)
+                    .commit();
+        }
+
+
+    }  //Main Method
+}  // Main Class
